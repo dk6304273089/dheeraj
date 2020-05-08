491461b1c1f15ef3d49aca4bad13795d4d0a41d6
dk6304273089 <65006527+dk6304273089@users.noreply.github.com>
2020-05-08 17:12:42 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n=int(input("enter a postive number:"))
+n1=0
+n2=1
+count=0
+while count <= n:
+    print(n1)
+    nth=n1+n2
+    n1=n2
+    n2=nth
+    count+=1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
