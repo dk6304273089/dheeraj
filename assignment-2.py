1a1e098ecd7c9477e5880369c47c4ad990ac851f
dk6304273089 <65006527+dk6304273089@users.noreply.github.com>
2020-05-08 17:37:05 +0530

    Add files via upload

---
+++ assignment-2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i >=0:
+        print(i,end=',')
+        
+
+
+# In[21]:
+
+
+f=[]
+list2=[12,14,-95,3]
+for j in list2:
+    if j >=0:
+        f.append(j)
+f
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
